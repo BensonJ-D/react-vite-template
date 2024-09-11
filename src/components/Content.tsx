16120357e4d7138405962cdbb492b8a650da0b46
James Benson <jamesdavidbenson@gmail.com>
2024-09-11 12:01:08 +1000

    Fix SSM commands not running, use remotely applied deploy script to instance.

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -7,14 +7,12 @@ export const Content = () => {
   const { getPageContent } = useApi();
 
   const [content, setContent] = useState<any>();
-  const [content2, setContent2] = useState<any>();
 
   const { data } = getPageContent('/coffee/hot');
 
   useEffect(() => {
     if (data) {
       setContent(data[0]);
-      setContent2(data[1]);
     }
   }, [data]);
 
@@ -23,10 +21,6 @@ export const Content = () => {
       <CssBaseline/>
       <ContentWrapper>
         {content?.title}
-        <br />
-        {content2?.title}
-        <br />
-        Yet another coffee: {data && data[2]?.title}
       </ContentWrapper>
     </>
   );
